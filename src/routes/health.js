0db098859599ef78a276cbd181634093ebb36c89
Luke Thilgen <luca.thilgen@gmail.com>
2025-12-03 08:49:25 -0800

    Updated routes to fit deployment

--- src/routes/health.js
+++ src/routes/health.js
@@ -4,6 +4,8 @@ router.get('/', (req, res) => {
     res.json({ status: 'ok', time: new Date().toISOString() });
 
 });
-
+router.get('/', (req, res) => {
+    res.json({status: 'ok '});
+});
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const connectDB = require("./config/db");
 const healthRouter = require('./routes/health')
 const studentsRouter = require('./routes/students');
 const app = express();
 
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use('/health', healthRouter)
 app.use('/api/students', studentsRouter)
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send("StudentConnect API is running. ");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
